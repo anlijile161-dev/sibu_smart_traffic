0ca5f8fd4b71b3d337e43ac77e14edbdf5b06937
anlijile161-dev <anlijile161@gmail.com>
2025-10-20 00:32:35 +0800

    Add files via upload

--- beta/sw.js
+++ beta/sw.js
@@ -1,2 +1,28 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { self.clients.claim(); });
+const CACHE='sst-beta-plus-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil((async()=>{
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith((async()=>{
+    try {
+      const res = await fetch(e.request);
+      return res;
+    } catch(err){
+      const cached = await caches.match(e.request);
+      if (cached) return cached;
+      if (e.request.mode === 'navigate') return caches.match('./index.html');
+      throw err;
+    }
+  })());
+});
